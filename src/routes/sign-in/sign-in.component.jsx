c0e76d38f0266700e15cd7f67784836f22bf1b80
blade-demon <xuziwei89@gmail.com>
2022-07-21 08:25:57 +0800

    add navigation style

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,12 @@
+const SignIn = () => {
+  const logGoogleUser = async () => {};
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
