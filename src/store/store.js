e57d0f77fc943e410c1495fa6756773ec883c76f
blade-demon <xuziwei89@gmail.com>
2022-08-14 06:20:42 +0800

    add redux selector

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,19 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
+
+//   console.log("type", action.type);
+//   console.log("payload", action.payload);
+//   console.log("currentState", store.getState());
+
+//   next(action);
+
+//   console.log("next state", store.getState());
+// };
 const middlewares = [logger];
 
 const composeEnhancers = compose(applyMiddleware(...middlewares));
