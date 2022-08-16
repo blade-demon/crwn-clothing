8eb3237ee3739650345ff8f4943291403341937c
xuziwei <xuziwei@xuziweideMacBook-Pro.local>
2022-08-16 23:24:57 +0800

    add redux-saga

--- src/store/store.js
+++ src/store/store.js
@@ -5,19 +5,29 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
-import thunkMiddleware from "redux-thunk";
+// import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from "react-saga";
+
+import { rootSaga } from "./root-saga";
 
 const persisitConfig = {
   key: "root",
   storage,
-  blacklist: ["cart"],
+  whitelist: ["cart"],
 };
 const persistedReducer = persistReducer(persisitConfig, rootReducer);
 
-const middlewares = [thunkMiddleware, logger];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [
+  process.env.NODE_ENV !== "PRODUCTION" && logger,
+  sagaMiddleware,
+].filter(Boolean);
 
 const composeEnhancers = compose(applyMiddleware(...middlewares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
